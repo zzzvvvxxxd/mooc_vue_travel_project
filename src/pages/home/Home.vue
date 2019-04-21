02acfc0161fbdd0127e1aef2303b145ea43c17d8
zhangweiqian <zhangweiqian@meituan.com>
2019-04-21 23:15:05 +0800

    初始化项目

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    Hello World!
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
