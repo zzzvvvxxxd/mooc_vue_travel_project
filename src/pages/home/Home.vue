08b17d52799e21d156b6fcb6592cef797caa2872
zhangweiqian <zhangweiqian@meituan.com>
2019-04-27 17:27:40 +0800

    金刚列表开发

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,15 +2,17 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
-    test
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   components: {
+    HomeIcons,
     HomeHeader,
     HomeSwiper
   },
