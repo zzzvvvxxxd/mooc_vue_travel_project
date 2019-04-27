e4183e19511a4833d375fc75b58f75fb481924b4
zhangweiqian <zhangweiqian@meituan.com>
2019-04-27 12:44:19 +0800

    fix

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <home-header></home-header>
     Hello World!
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
+  components: {
+    HomeHeader
+  },
   name: 'Home'
 }
 </script>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <div class="iconfont iconleft back-icon"></div>
+      </div>
+      <div class="header-input">
+        <span class="iconfont iconsearch"></span> 输入城市/景点/游玩主体
+      </div>
+      <div class="header-right">
+        城市
+        <span class="iconfont iconcaret-down arrow-down-icon"></span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+.header
+  display: -webkit-flex;
+  display: flex
+  height: .86rem
+  background: $mainColor
+  color: #fff
+  line-height: .86rem
+  .header-left
+    width: .64rem
+    float: left
+    .back-icon
+      text-align center
+  .header-input
+    flex: 1
+    height: .64rem
+    line-height: .64rem
+    border-radius: .1rem
+    margin-top: .12rem
+    margin-left: .2rem
+    background-color: #fff
+    color: #ccc
+    padding-left .2rem
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align center
+    .arrow-down-icon
+      margin-left: -.04rem
+      font-size: .24rem
+</style>
